d115ce391b9fea7e2d5f9f9fea4405a9daed7263
cstanescu123 <corneliusemail@gmail.com>
2022-01-10 18:09:08 -0500

    lab 11

---
+++ Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movie
+{
+    internal class Movie
+    {
+        public Movie()
+        {
+        }
+
+        public Movie(string title, string category)
+        {
+            Category = category;
+            Title = title;
+        }
+        public string Category { get; set; }
+        public string Title { get; set; }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace movie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie("The Simpsons Movie", "Animated"));
+            movies.Add(new Movie("Aladdin", "Animated"));
+            movies.Add(new Movie("Saw", "Horror"));
+            movies.Add(new Movie("Saw 2", "Horror"));
+            movies.Add(new Movie("Gangs of New York", "Drama"));
+            movies.Add(new Movie("The Prestige", "Drama"));
+            movies.Add(new Movie("Star Wars", "Scifi"));
+            movies.Add(new Movie("Star Trek", "Scifi"));
+
+            foreach (var movie in movies)
+
+                Console.WriteLine("What category are you interested in?");
+            string userInput = Console.ReadLine();
+
+
+
+            Console.WriteLine("Continue? (y/n)");
+            string userInput1 = Console.ReadLine();
+
+
+
+        }
+    }
+}
